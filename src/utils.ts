bae264e2d6f8949bf14ce07b66aeb324e0f1451e
SUHO PARK <duglikespiano@gmail.com>
2025-01-21 17:54:39 +0900

    FEATURE - SORT FUNCTION FOR ARTICLES BY DATE ADDED

---
+++ src/utils.ts
@@ -0,0 +1,6 @@
+const formatDate = (date: Date) => {
+	const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+	return new Date(date).toLocaleDateString(undefined, options);
+};
+
+export { formatDate };
